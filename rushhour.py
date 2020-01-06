1250cd90eed8c80c9ebf86860172457fa93789db
Jeroen Struijk <32066603+JeroenStruijk@users.noreply.github.com>
2020-01-06 10:41:59 +0100

    Timer visible on screen
    Timer now visible on rushhour tab, need to fix restart and possible starting the timer when mouseclick.

--- rushhour.py
+++ rushhour.py
@@ -1,16 +1,18 @@
+import time
 
 def setup():
     #laad de uitleg van rushhour en de template achtergrond in en resized deze naar het juiste formaat.
-    global img, template
+    global img, template, timer, start
     img = loadImage("Images/rushhour.png")
     template = loadImage("Images/template.jpg")
     img.resize(450,650)
     template.resize(750,750)
     frameRate(1)
+    start = time.time()
     
     
 def draw():
-    global start,timer
+    global img, template, timer, start
     image(template, 0, 0)
     image(img, 150,10)
     
@@ -20,19 +22,24 @@ def draw():
     strokeWeight(3)
     rect(149, 9, 452, 652)
     
-    
-    
-    
-def draw():
-    global start,font, timer
-    textFont(font)
-    background(230)
+    #timergerelateerde text + rectangles
     timer = countdown(start)
     fill(0)
-    text(str(convert(int(timer))), 500, 70)
-
-
-
+    textSize(30)
+    text(str(convert(int(timer))), 30, 375)
+    text(str(convert(int(timer))), 630, 375)
+    noFill()
+    stroke(0)
+    strokeWeight(3)
+    rect(27, 344, 100, 40)
+    rect(627, 344, 100, 40)
+    
+    #overige tekst/instructies
+    textSize(20)
+    text("Tijd over:", 29, 336)
+    text("Tijd over:", 629, 336)
+    textSize(14)
+    text("Wanneer de tijd om is is het spel afgelopen!", 159, 570)
 
 
 def countdown(start):
