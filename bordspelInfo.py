5e5284cd4d83371f3bd071837d856cb23e57b9e7
Jeroen Struijk <32066603+JeroenStruijk@users.noreply.github.com>
2020-01-07 14:48:34 +0100

    Spellingcheck uitgevoerd en waar nodig interpunctie en spelling aangepast.

--- bordspelInfo.py
+++ bordspelInfo.py
@@ -18,7 +18,7 @@ def draw(bordspelGrid):
     if bordspelGrid == -1: # West-Berlijn
         infoPage = 7
         textSize(28)
-        text("West-Berlijn Vak", 120, 125)
+        text("Thuisbasis West-Berlijn", 120, 125)
         textSize(21)
         text("Alle spionnen van West-Berlijn (blauwe pionnen)\n"+
              "beginnen in het vak van de thuisbasis van\n" +
@@ -26,7 +26,7 @@ def draw(bordspelGrid):
     elif bordspelGrid == -2: # Oost-Berlijn
         infoPage = 7
         textSize(28)
-        text("Oost-Berlijn Vak", 120, 125)
+        text("Thuisbasis Oost-Berlijn", 120, 125)
         textSize(21)
         text("Alle spionnen van Oost-Berlijn (rode pionnen)\n"+
              "beginnen in het vak van de thuisbasis van\n"+
@@ -34,31 +34,31 @@ def draw(bordspelGrid):
     elif bordspelGrid == -3: # Ziekenhuis
         infoPage = 9
         textSize(28)
-        text("Ziekenhuis Vak", 120, 125)
+        text("Ziekenhuis", 120, 125)
         textSize(21)
         text("Als je op dit vak loopt kun je een gewonde\n"+
              "spion helen.", 120, 180)
     elif bordspelGrid == -4: # Muur
         infoPage = 10
         textSize(28)
-        text("Muur Vak", 120, 125)
+        text("Berlijnse muur", 120, 125)
         textSize(21)
         text("Een pion kan over de muur gaan door 3 te\n"+
              "gooien. Als je 2 gooit blijf je staan en\n"+
-             "als 1 gooi raak je gewond.", 120, 180)
+             "als je 1 gooit raak je gewond.", 120, 180)
     elif bordspelGrid == -5: # Speciaal
         infoPage = 8
         textSize(28)
-        text("Speciaal Vak", 120, 125)
+        text("Speciale kaart", 120, 125)
         textSize(21)
         text("Als je op dit vak komt mag je een speciale\n"+
              "kaart pakken.", 120, 180)
     elif bordspelGrid == -6: # Tunnel
         infoPage = 12
         textSize(28)
-        text("Tunnel Vak", 120, 125)
+        text("Tunnel", 120, 125)
         textSize(21)
-        text("Je kunt dit gebruiken om aan de andere \n"
+        text("Je kunt door de tunnel om aan de andere \n"
              "kant van de Berlijnse muur te komen zonder\n"+
              "het risico te lopen gewond te raken.", 120, 180)
     else: # Leeg
